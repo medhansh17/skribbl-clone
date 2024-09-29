799dabff48ab7f23581943f7c239c0949fd4c84e
Medhansh Jain <medhanshjain2010@gmail.com>
2024-09-29 16:48:05 +0530

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import { Server } from "socket.io";
+import http from "http";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get("/", function (req, res) {
+  res.sendFile(`${process.cwd()}/static/index.html`);
+});
+
+io.on("connection", (socket) => {
+  console.log("New Connection", socket.id);
+});
+
+server.listen(3000, function () {
+  console.log("listening on *:3000");
+});
